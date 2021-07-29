7998c9a50fc4f61e4e600921e23d19607d1ac76b
vardhan <harshwardhankalse03@gmail.com>
2021-07-29 17:42:07 +0530

    Added Routers, switch aand path to the components in App.js

--- src/App.js
+++ src/App.js
@@ -3,30 +3,35 @@ import ItemCard from "./Components/Item";
 import { Container, Row, ProgressBar } from "react-bootstrap";
 import NavBar from "./Components/Navbar";
 import useFetch from "./FetchApi/useFetch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   const { data, loading, error } = useFetch("");
   return (
-    <>
+    <Router>
       <NavBar />
-      <Container className="App">
-        <Row style={{ position: "relative" }}>
-          {loading && (
-            <ProgressBar
-              now={60}
-              style={{
-                margin: "auto",
-              }}
-              label="Loading..."
-            />
-          )}
-          {error && <h1>Error, Try Again !</h1>}
-          {data.map((data) => {
-            return <ItemCard key={data.id} data={data} />;
-          })}
-        </Row>
-      </Container>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Container className="App">
+            <Row style={{ position: "relative" }}>
+              {loading && (
+                <ProgressBar
+                  now={60}
+                  style={{
+                    margin: "auto",
+                  }}
+                  label="Loading..."
+                />
+              )}
+              {error && <h1>Error, Try Again !</h1>}
+              {data.map((data) => {
+                return <ItemCard key={data.id} data={data} />;
+              })}
+            </Row>
+          </Container>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
