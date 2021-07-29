87d3a15bb879c4716ad5739ec3f0a6ebf4ac93d3
vardhan <harshwardhankalse03@gmail.com>
2021-07-29 15:16:22 +0530

    Called Data from useFetch cusom hook -added Linear Loader - Mapping data over itemCard Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import React from "react";
+import ItemCard from "./Components/Item";
+import { Container, Row, ProgressBar } from "react-bootstrap";
+import NavBar from "./Components/Navbar";
+import useFetch from "./FetchApi/useFetch";
+
 function App() {
-  return <div className="App"> </div>;
+  const { data, loading, error } = useFetch();
+  return (
+    <>
+      <NavBar />
+      <Container className="App">
+        <Row style={{ position: "relative" }}>
+          {loading && (
+            <ProgressBar
+              now={60}
+              style={{
+                margin: "auto",
+              }}
+              label="Loading..."
+            />
+          )}
+          {error && <h1>Error, Try Again !</h1>}
+          {data.map((data) => {
+            return <ItemCard key={data.id} data={data} />;
+          })}
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
-// Api To USE : https://fakestoreapi.com/products
