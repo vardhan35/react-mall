3a9f0c6f31ce0994de79aa54a83ba57e490a55d1
vardhan <harshwardhankalse03@gmail.com>
2021-07-29 15:15:01 +0530

    useFetch.js file Created Adding reducer and fetching api using axios

---
+++ src/FetchApi/useFetch.js
@@ -0,0 +1,36 @@
+import { useReducer, useEffect } from "react";
+import axios from "axios";
+import fetchReducer from "./Reducer";
+import {
+  FETCH_API_REQUEST,
+  FETCH_API_SUCCESS,
+  FETCH_API_FAILURE,
+} from "./ActionTypes";
+const BASE_URL = "https://fakestoreapi.com/products/";
+
+export default function useFetch(params) {
+  const [state, dispatch] = useReducer(fetchReducer, {
+    data: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+    dispatch({ type: FETCH_API_REQUEST });
+    axios
+      .get(BASE_URL, {
+        params: { ...params },
+      })
+      .then((response) => {
+        dispatch({
+          type: FETCH_API_SUCCESS,
+          payload: { data: response.data },
+        });
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) return;
+        dispatch({ type: FETCH_API_FAILURE, payload: { error: error } });
+      });
+  }, [params]);
+
+  return state;
+}
